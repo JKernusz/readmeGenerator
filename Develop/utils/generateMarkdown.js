c205d3eeac98dc7725f9a11ecbe6a814d3499075
John Kay <jkernusz1346@gmail.com>
2023-11-10 18:44:41 -0500

    updated generateMarkdown.js file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,61 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
+  } else {
+    return "";
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+ if (license!== "None") {
+  return `https://opensource.org/licenses/${license}`;
+ } else {
+  return "";
+  }
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  if (license!== "None") {
+    return `## License`;
+    } else {
+      return "";
+    }
+  }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Deployed Application URL
+${data.link}
+## Screenshot
+![alt-text](${data.screenshot})
+## Table of Contents
+* [Features](#features)
+* [Languages & Dependencies](#languagesanddependencies)
+* [How to Use This Application](#HowtoUseThisApplication)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [Questions](#questions)
+## Features
+${data.features}
+## Languages & Dependencies
+${data.require}
+## How to Use This Application:
+${data.usage}
+## Contributors
+${data.contributors}
+## Testing
+${data.test}
+## Questions
+Please send your questions [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect) or visit [github/${data.creator}](https://github.com/${data.creator}).
 
 `;
 }
